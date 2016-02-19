c4b8962e802d60d90347491aeb0bfc873ad6ca7a
Sergio Ferrer <14470223@itcampeche.edu.mx>
2016-02-19 07:56:20 -0600

    SimuladorContadorManual: Se agregó el manejo de eventos a la interfaz de usuario gráfica

--- src/gui/MainForm.java
+++ src/gui/MainForm.java
@@ -5,16 +5,20 @@
  */
 package gui;
 
+import contadormanual.ContadorManual;
+
+
+
 /**
  *
  * @author snayper
  */
 public class MainForm extends javax.swing.JFrame {
-
-    /**
-     * Creates new form MainForm
-     */
+         ContadorManual cm = new ContadorManual();
+      
     public MainForm() {
+       
+  
         initComponents();
     }
 
@@ -28,72 +32,122 @@ public class MainForm extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        CampoConteo = new javax.swing.JTextField();
+        botonContar = new javax.swing.JButton();
+        botonReiniciar = new javax.swing.JButton();
+        botonCerrar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Simulador de Contador Manual");
 
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
-        jTextField1.setText("jTextField1");
+        CampoConteo.setText("0");
+        CampoConteo.setFocusable(false);
+        CampoConteo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CampoConteoActionPerformed(evt);
+            }
+        });
 
-        jButton1.setText("jButton1");
+        botonContar.setText("Contar");
+        botonContar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonContarActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("jButton2");
+        botonReiniciar.setText("Reiniciar");
+        botonReiniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonReiniciarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTextField1)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(39, 39, 39)
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
-                .addComponent(jButton2)
-                .addGap(75, 75, 75))
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(CampoConteo)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(botonContar)
+                        .addGap(18, 18, 18)
+                        .addComponent(botonReiniciar)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {botonContar, botonReiniciar});
+
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(57, 57, 57)
+                .addContainerGap()
+                .addComponent(CampoConteo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addContainerGap(88, Short.MAX_VALUE))
+                    .addComponent(botonContar)
+                    .addComponent(botonReiniciar))
+                .addGap(39, 39, 39))
         );
 
-        jButton3.setText("jButton3");
+        botonCerrar.setText("Cerrar");
+        botonCerrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonCerrarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jButton3)))
-                .addContainerGap())
+                .addGap(135, 135, 135)
+                .addComponent(botonCerrar)
+                .addContainerGap(16, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 23, Short.MAX_VALUE)
-                .addComponent(jButton3)
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(botonCerrar, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void CampoConteoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CampoConteoActionPerformed
+      
+    }//GEN-LAST:event_CampoConteoActionPerformed
+
+    private void botonContarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonContarActionPerformed
+        
+        cm.contar();
+        CampoConteo.setText("" + cm.getConteoActual());
+        
+        
+        
+  
+    }//GEN-LAST:event_botonContarActionPerformed
+
+    private void botonReiniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonReiniciarActionPerformed
+        cm.reiniciarConteo();
+        CampoConteo.setText("" + cm.getConteoActual());
+    }//GEN-LAST:event_botonReiniciarActionPerformed
+
+    private void botonCerrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonCerrarActionPerformed
+       System.exit(0);
+    }//GEN-LAST:event_botonCerrarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -131,10 +185,10 @@ public class MainForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JTextField CampoConteo;
+    private javax.swing.JButton botonCerrar;
+    private javax.swing.JButton botonContar;
+    private javax.swing.JButton botonReiniciar;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
--- src/simuladorcontadormanual/Main.java
+++ src/simuladorcontadormanual/Main.java
@@ -5,6 +5,7 @@
 package simuladorcontadormanual;
 
 import contadormanual.ContadorManual;
+import gui.MainForm;
 
 /**
  *
@@ -16,8 +17,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ContadorManual contadorDePasajeros = new ContadorManual();
         
+        MainForm ventana = new MainForm();
+        ventana.setVisible(true);
+        /*ContadorManual contadorDePasajeros = new ContadorManual(); 
         System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
         contadorDePasajeros.contar();
         System.out.println("Click");
@@ -30,6 +33,6 @@ public class Main {
         System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
         System.out.println("Se reiniciará el conteo de pasajeros...");
         contadorDePasajeros.reiniciarConteo();
-        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
+        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());*/
     }
 }
